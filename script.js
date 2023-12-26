787ec1725b0fd28b6a1f5a233d4c74bdb4a3d404
rolansy <ronalshoeygeorge@gmail.com>
2023-12-26 22:02:36 +0530

    Add script.js file with code changes

---
+++ script.js
@@ -0,0 +1,71 @@
+console.log("Hello World!")
+console.log("yo")
+console.log(23  )
+var x=46
+var c="char"
+console.log(x)
+console.log(c)
+
+function hello(){
+    if (true){
+        let a="AA"
+        var b="BB"
+        const c="CC"
+        console.log(a)
+        console.log(b)
+        console.log(c)
+
+    }
+    console.log(b)
+    // console.log(a)
+    // console.log(c)
+
+}
+
+hello()
+const a=45
+var b=69
+b="hello"
+console.log(typeof b)
+console.log(typeof a)
+console.log(b)
+console.log(a)
+console.log(c)
+
+function logger(){
+    let a=10
+    let b=25
+    console.log(a+b)
+
+}
+
+logger()
+logger()
+console. log("----")
+console. log("----")
+console. log("----") 
+console. log("----")
+logger( )
+
+function expo(a,b){
+    console.log(a**b)
+}
+
+expo(2,3)
+
+function fruit(a,b){
+    console.log(`fruit juice of ${a} apples and ${b} oranges`)
+
+}
+
+fruit(2,3)
+
+function retsamp(){
+    console.log("before")
+    return 12
+    console.log("after")
+
+}
+r=retsamp()
+console.log(r   )
+console.log(retsamp())
